f91edb2e566b64203bf0bdc6326ab988b6775d67
Piotr Boruc <piotr.m.boruc@gmail.com>
2026-01-22 14:54:44 +0100

    refactor: switch to universal binary
    - Remove architecture-specific builds
    - Use single universal binary for both Intel and Apple Silicon
    - Simplifies installation and avoids Gatekeeper issues
    - Slightly larger file size but better compatibility

--- Casks/kube-ingress-launcher.rb
+++ Casks/kube-ingress-launcher.rb
@@ -1,11 +1,8 @@
 cask "kube-ingress-launcher" do
   version "0.1.0"
   
-  arch arm: "aarch64", intel: "x86_64"
-  
-  url "https://github.com/wasilak/kube-ingress-launcher/releases/download/v#{version}/kube-ingress-launcher-#{version}-#{arch}-apple-darwin.dmg"
-  sha256 arm:   "b691ac66e42c60a08084ae2fd4efc88c4c58f8cb197e72d3a7f555bb9a773ff4",
-         intel: "8f33414a851b865ae9fa887e40692148e9a095ad3b2b0ad1175d7356df651913"
+  url "https://github.com/wasilak/kube-ingress-launcher/releases/download/v#{version}/kube-ingress-launcher-#{version}-universal-apple-darwin.dmg"
+  sha256 "PLACEHOLDER_UNIVERSAL_SHA256"
   
   name "Kube Ingress Launcher"
   desc "Desktop launcher for quickly searching and opening Kubernetes ingress resources"
